d3d709399b529f4cee21017c9fce9fa84d29fc0a
Nadim <nahlenadim@gmail.com>
2022-06-09 08:15:45 +0300

    API Create- Register

--- laravel-server/app/Http/Controllers/AuthController.php
+++ laravel-server/app/Http/Controllers/AuthController.php
@@ -10,5 +10,25 @@ use App\Models\User;
 
 class AuthController extends Controller
 {   
-    //
+    public function _construct(){
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+    }
+    public function register(Request $request){
+        $validator = Validator::make($request -> all(), [
+            'name' => 'required',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ]);
+        if($validator -> fails()){
+            return response()->json($validator->errors()->toJson(),400);
+        }
+        $user = User::create(array_merge(
+            $validator->validated(),
+            ['password'=>bcrypt($request->password)]
+        ));
+        return response()->json([
+            'message' => 'User success',
+            'user' => $user
+        ],201);
+    }
 }
--- laravel-server/routes/api.php
+++ laravel-server/routes/api.php
@@ -15,17 +15,27 @@ use App\Http\Controllers\ItemController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//VERSION ---1---
 Route::group(['prefix' => 'v1'], function (){
 
-
+    //ITEMS GROUP
     Route::group(['prefix' => 'items'], function (){
     
         Route::get('/allitems', [ItemController::class, 'getAllItems']);
         Route::get('/item/{id?}', [ItemController::class, 'getItem']);
+       
         
     });
     
-   
+    //USERS GROUP
+    Route::group(['prefix' => 'user'], function (){
+
+        Route::group(['middleware'=>'api', 'prefix' => 'auth'],function($router){
+
+            Route::post('/register', [AuthController::class, 'register']);
+                       
+        });
+    
+    });
     
 });
