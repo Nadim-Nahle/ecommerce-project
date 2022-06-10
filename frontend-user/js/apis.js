9f79046956f42118ba6f1024cc845d25ae929ceb
Nadim <nahlenadim@gmail.com>
2022-06-10 10:17:36 +0300

    JS Create- Login

--- frontend-user/js/apis.js
+++ frontend-user/js/apis.js
@@ -22,10 +22,47 @@ if( signupBtn){
       console.log(result.message);
       let message = result.status;
       if(result.message){
-        location.href = 'http://localhost/food-lounge/login.html';
+        location.href = 'file:///C:/xampp/htdocs/ecommerce-project/frontend-user/signin.html';
      };
 
                 
     })
 })
 }
+
+
+
+//Login script
+const loginBtn = document.querySelector("#login-btn");
+const emailLogin = document.getElementById("email-login");
+const passwordlogin = document.getElementById("password-login");
+
+  loginBtn.addEventListener("click", function (event) {
+ 
+    event.preventDefault();
+     let data = new FormData();
+
+     data.append('email', emailLogin.value);
+     data.append('password', passwordlogin.value);
+     
+     axios({
+       method: 'post',
+       url: 'http://127.0.0.1:8000/api/v1/user/auth/login',
+       data: data,
+   }).then(function (response) {
+        
+        let result = (response.data);
+        //console.log(result.token_type);
+        if(result.token_type == 'bearer'){
+          location.href = 'file:///C:/xampp/htdocs/ecommerce-project/frontend-user/index.html';
+        }
+        
+        
+       
+
+   })
+     
+
+});
+
+
