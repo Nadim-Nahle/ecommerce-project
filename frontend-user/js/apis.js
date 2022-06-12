3bc383d48b8a41d15e44a4df37be9a1d024713f8
Nadim <nahlenadim@gmail.com>
2022-06-12 10:00:25 +0300

    Frontend JS Add- GetItems

--- frontend-user/js/apis.js
+++ frontend-user/js/apis.js
@@ -36,7 +36,7 @@ if( signupBtn){
 const loginBtn = document.querySelector("#login-btn");
 const emailLogin = document.getElementById("email-login");
 const passwordlogin = document.getElementById("password-login");
-
+if(loginBtn){
   loginBtn.addEventListener("click", function (event) {
  
     event.preventDefault();
@@ -57,12 +57,78 @@ const passwordlogin = document.getElementById("password-login");
           location.href = 'file:///C:/xampp/htdocs/ecommerce-project/frontend-user/index.html';
         }
         
-        
-       
+              
 
    })
      
 
 });
+}
+
+
+
+//Get Items Script
+let nxtBtns = document.querySelector(".nxt-btn")
+if(nxtBtns){
+  window.addEventListener('load', (event) => {
+    let data = new FormData();
+    axios({
+      method: 'get',
+      url: 'http://127.0.0.1:8000/api/v1/items/item',
+      data: data,
+  })
+      .then(function(response) {
+        
+        let result=(response.data);
+        //console.log(result.items.length);
+        for (let i =0; i<result.items.length; i++){
+          newName = (result.items[i].name);
+          console.log(newName);
+          newDescription =(result.items[i].detail);
+          console.log(newDescription);
+          newPic =(result.items[i].pic_link);
+          console.log(newPic);
+          
+          const itemContainer = document.querySelector(".item-container");
+          const itemCard = document.createElement("div");
+          itemCard.classList.add("item-card");
+
+          const itemImage = document.createElement("div");
+          itemImage.classList.add("item-image");
+          
+
+          const itemInfo = document.createElement("div");
+          itemInfo.classList.add("item-info");
+
+          const a = document.createElement("a");
+
+          const img = document.createElement("img");
+          img.classList.add("item-thumb");
+          img.src = newPic;
+
+          const h2 = document.createElement("h2");
+          h2.classList.add("item-name");
+          h2.innerHTML=newName;
+
+          const p1 = document.createElement("p");
+          p1.classList.add("item-short-description");
+          p1.innerHTML=newDescription;
+
+          itemContainer.appendChild(itemCard);
+          itemCard.appendChild(itemImage);
+          itemImage.appendChild(a);
+          a.appendChild(img);
+          itemCard.appendChild(itemInfo);
+          itemInfo.appendChild(h2);
+          itemInfo.appendChild(p1);
+          a.setAttribute('href', "review.html")
+        }
+        
+      })
+
+               
+  })
+
+}
 
 
